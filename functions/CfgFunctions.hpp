c1e67eeb8d41a3d25b82df666a024b0e1a78a297
Luke <bob_monday@outlook.com>
2017-07-04 23:44:45 +0100

    Add files via upload

---
+++ functions/CfgFunctions.hpp
@@ -0,0 +1,17 @@
+/*
+    filename: @CfgFunctions.hpp
+    Author: Luke(DOD)
+    coded for invade and annex tanoa (DOD).
+	Can be used as template for any mission.
+*/
+
+
+class Luke
+{
+	tag = "BOB";
+	class functions
+	{
+		file = "functions";
+        myFirstFunction {};
+	};
+};
