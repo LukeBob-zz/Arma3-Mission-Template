49b86f41a2e6ac19e3233fbcf5be5214f6df9cc7
Luke <bob_monday@outlook.com>
2017-07-04 23:52:22 +0100

    Update CfgFunctions.hpp

--- functions/CfgFunctions.hpp
+++ functions/CfgFunctions.hpp
@@ -2,16 +2,27 @@
     filename: @CfgFunctions.hpp
     Author: Luke(DOD)
     coded for invade and annex tanoa (DOD).
-	Can be used as template for any mission.
+    Can be used as template for any mission.
+    
+    class name can be anything.
+    tag name is important.
+    
+    call or spawn with [] call\spawn MYTAG_fnc_MYFUNCTION;
+    
+    "MYTAG" being your tag
+    and MYFUNCTION the name you defined the function in CfgFunction.hpp.
+    
+    example below would be [object, "my animation"] call BOB_fnc_myFirstFunction;
+   
 */
 
 
 class Luke
 {
-	tag = "BOB";
-	class functions
-	{
-		file = "functions";
+    tag = "BOB";
+    class functions
+    {
+        file = "functions";
         myFirstFunction {};
-	};
+    };
 };
